f734e557fb6bc67e053e3a86a791fa1e41503623
tomas <72792707+tomas-land@users.noreply.github.com>
2023-02-03 10:32:02 +0200

    first commit

---
+++ app/Comp.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React from 'react'
+
+const Comp = ({data}:any) => {
+  console.log(data);
+  return (
+    <div>
+      {data.map((item:any) => {
+        return (
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+            <ul>
+              {/* {item.CategoriesOnExpenses.map((item:any) => {
+                return (
+                  <li key={item.id}>
+                    <p>{item.Expense.amount}</p>
+                  </li>
+                )
+              })} */}
+            </ul>
+          </div>
+        )
+      })
+      }
+    </div>
+  )
+}
+
+export default Comp
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Comp from './Comp'
+import { prisma } from 'lib/prisma/db'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const getData = async () => {
+  const data = await prisma.category.findMany({
+    include: {
+      CategoriesOnExpenses: true
+    }
+  })
+  return JSON.parse(JSON.stringify(data));
+}
+
+export default async function Home() {
+  const data = await getData()
+  console.log(data)
   return (
     <main className={styles.main}>
-    
+      <h1>test</h1>
+      <Comp data={data} />
     </main>
   )
 }
